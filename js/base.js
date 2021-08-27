3de8b6158c1352d069f39b58216d0dd0e49c9891
NaeemBolchhi <42773187+NaeemBolchhi@users.noreply.github.com>
2021-08-27 18:55:42 +0600

    Add files via upload

--- js/base.js
+++ js/base.js
@@ -11,20 +11,43 @@
 
 // Hide loading screen after 1.3 seconds.
 function fadeOut(i) {
+document.getElementById(i).style.opacity = 1;
 let x; let y; var z;
   function xyz() {
     y = document.getElementById(i).style.opacity;
-    z = y - 0.0035;
+    z = y - 0.002;
     document.getElementById(i).style.opacity = z;
   }
   setTimeout(function(){
-    for (x = 0; x < 280; x++) {
+    for (x = 0; x < 500; x++) {
       setTimeout(xyz, x);
     }},1000);
   setTimeout(function(){
     document.getElementById(i).style.display = "none";
-  },1300);
+  },1500);
 }
-
-// Call all functions.
 fadeOut('top');
+
+// Particle background settings.
+window.onload = function() {
+  Particles.init({
+    selector: '.background',
+    color: '#D7C5F9',
+	sizeVariations: 5,
+    maxParticles: 120,
+    connectParticles: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        options: {
+        maxParticles: 80
+        }
+      }, {
+        breakpoint: 375,
+        options: {
+        maxParticles: 50
+        }
+      }
+   ]
+  });
+};
